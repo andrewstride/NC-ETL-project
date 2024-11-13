9deaa7593ddbe14c14064621801a7296b6bde17f
vendettaaaa <jamesacks1999@gmail.com>
2024-11-13 09:12:35 +0000

    added main to brances

--- test/itegration/test_db_conn.py
+++ test/itegration/test_db_conn.py
@@ -19,10 +19,6 @@ def connect_to_db():
     )
     return connection
 
-# def test_db_connection():
-#     conn = connect_to_db()
-#     assert conn is not None  # Simple test to ensure connection is established
-#     # You can add more assertions or test logic here as needed
 
 def test_db_connection():
     conn = connect_to_db()
