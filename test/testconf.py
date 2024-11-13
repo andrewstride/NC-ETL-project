384fe4dea854c98e540f089e28d06ff50a9e1ddf
Andrew Stride <andrewjonathanstride@gmail.com>
2024-11-13 16:46:09 +0000

    sql injection prevention using tables name constraint

---
+++ test/testconf.py
@@ -0,0 +1,10 @@
+import os
+import pytest
+
+@pytest.fixture()
+def aws_cred():
+    os.environ["AWS_ACCESS_KEY_ID"] = "test"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "test"
+    os.environ["AWS_SECURITY_TOKEN"] = "test"
+    os.environ["AWS_SESSION_TOKEN"] = "test"
+    os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
