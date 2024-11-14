036f33b1f6dd53a0a0aea1eaf5ece6fe2c62aa14
studiobigli <acuddlytoy@googlemail.com>
2024-11-14 11:16:24 +0000

    archive_file waits for null_resources to run

--- terraform/lambda_layers.tf
+++ terraform/lambda_layers.tf
@@ -1,18 +1,26 @@
 ## pip install requirements for lambda1 (from requirements-lambda1.txt file)
 resource "null_resource" "create_dependencies" {
     provisioner "local-exec" {
-        command = "pip install -r ${path.module}/../requirements-${var.lambda1_name}.txt -t ${path.module}/../dependencies-${var.lambda1_name}/python"
+        command = "pip install -r ${path.module}/../requirements-${var.lambda1_name}.txt -t ${path.module}/${var.lambda1_layer_deployment_dir}/python"
     }
     triggers = {
         dependencies = filemd5("${path.module}/../requirements-${var.lambda1_name}.txt")
     }
 }
 
+## prevent archive_file taking place before "create_dependencies occurs"
+data "null_data_source" "wait_for_create_dependencies" {
+  inputs = {
+    create_dependencies_id = "${null_resource.create_dependencies.id}"
+    source_dir = "${path.module}/${var.lambda1_layer_deployment_dir}"
+  }
+}
 ## zip file for lambda1 layer - getting the dependecies file locally and zipping it
 data "archive_file" "layer_code_for_lambda1" {
     type = "zip"
-    output_path = "${path.module}/../packages/layer/layer.zip"
-    source_dir = "${path.module}/../dependencies-${var.lambda1_name}"
+    output_path = "${path.module}/../terraform-remote-deployment/lambda1_layer.zip"
+    source_dir  = "${data.null_data_source.wait_for_create_dependencies.outputs["source_dir"]}"
+    #source_dir = "${path.module}/${var.lambda1_layer_deployment_dir}"
 }
 
 ## Create lambda1 layer from the zip file
