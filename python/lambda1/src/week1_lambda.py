d77a6a261ea90d9b3981c8725c3550b7bff46eff
Andrew Stride <andrewjonathanstride@gmail.com>
2024-11-15 10:21:27 +0000

    basic data writing functionality implimented, test mocking not currently working

--- python/lambda1/src/week1_lambda.py
+++ python/lambda1/src/week1_lambda.py
@@ -1,22 +1,28 @@
 from src.connection import db_connection
 from src.utils import get_all_rows, get_columns, write_to_s3, get_tables
+from datetime import datetime
+import json
 import logging
 import boto3
-import json
 
 logger = logging.getLogger()
 
 def lambda_handler(event, context):
     try:
         conn = db_connection()
-        rows = get_all_rows(conn, "staff")
-        columns = get_columns(conn, "staff")
-        logger.error("Houston, we have a %s", "major problem", exc_info=True)
-        staff_table = json.dumps(columns.append(rows))
         s3 = boto3.resource('s3')
-        response = write_to_s3(s3, 'nc-terraformers-ingestion', 'staff', 'csv', staff_table)
-        return {'response': 200,
-                'body': response}
+        for table in get_tables(conn):
+            rows = get_all_rows(conn, table)
+            columns_and_rows = [get_columns(conn, table)]
+            for row in rows:
+                columns_and_rows.append(row)
+            timestamp = str(datetime.now())
+            filename = f"{table}/{table}_{timestamp}"
+            data = str(columns_and_rows)
+            write_to_s3(s3, 'nc-terraformers-ingestion', filename, 'csv', data)
+        logger.error("Houston, we have a %s", "major problem", exc_info=True)  
+        return {'response': 200}
+
     except Exception as e:
         logging.error(e)
         return {'response': 500,
