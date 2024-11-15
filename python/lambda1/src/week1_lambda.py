fd9ca4523b5d019a2437c3ce2bd843cd7707583c
Andrew Stride <andrewjonathanstride@gmail.com>
2024-11-15 10:33:43 +0000

    write timestamp dict to bucket

--- python/lambda1/src/week1_lambda.py
+++ python/lambda1/src/week1_lambda.py
@@ -1,5 +1,5 @@
 from src.connection import db_connection
-from src.utils import get_all_rows, get_columns, write_to_s3, get_tables
+from src.utils import get_all_rows, get_columns, write_to_s3, get_tables, fetch_last_timestamps_from_db, get_new_rows
 from datetime import datetime
 import json
 import logging
@@ -11,6 +11,8 @@ def lambda_handler(event, context):
     try:
         conn = db_connection()
         s3 = boto3.resource('s3')
+        timestamp_dict = json.dumps(fetch_last_timestamps_from_db(conn))
+        write_to_s3(s3, 'nc-terraformers-ingestion', 'timestamp_table', 'json', timestamp_dict)
         for table in get_tables(conn):
             rows = get_all_rows(conn, table)
             columns_and_rows = [get_columns(conn, table)]
