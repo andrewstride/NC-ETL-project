c96745e31a9b7d88f5da03f2270b0be00030e049
studiobigli <acuddlytoy@googlemail.com>
2024-11-18 14:44:14 +0000

    Add processing bucket, add lambda2 uploads to codebucket

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,7 +1,6 @@
 ## S3 bucket to store lambda code
 resource "aws_s3_bucket" "code_bucket" {
     bucket = "nc-terraformers-code"
-
     tags = {
         name = "code_bucket"
         environment = "dev"
@@ -27,14 +26,40 @@ resource "aws_s3_object" "lambda1_layer" {
   depends_on = [ data.archive_file.layer_code_for_lambda1 ]
 }
 
+## zip file for lambda2 in S3 code bucket
+resource "aws_s3_object" "lambda2" {
+  bucket = aws_s3_bucket.code_bucket.bucket
+  key    = "${var.lambda2_name}/${var.lambda2_name}.zip"
+  source = data.archive_file.lambda2_handler_func.output_path
+  etag   = filemd5(data.archive_file.lambda2_handler_func.output_path)
+  depends_on = [ data.archive_file.lambda2_handler_func ]
+}
+
+## zip file for lambda2 layer in S3 code bucket
+resource "aws_s3_object" "lambda2_layer" {
+  bucket = aws_s3_bucket.code_bucket.bucket
+  key    = "${var.lambda2_name}_layer/layer.zip"
+  source = data.archive_file.layer_code_for_lambda2.output_path
+  etag   = filemd5(data.archive_file.layer_code_for_lambda2.output_path)
+  depends_on = [ data.archive_file.layer_code_for_lambda2 ]
+}
 
 ## Ingestion S3 bucket
 resource "aws_s3_bucket" "ingestion_bucket" {
     bucket = "nc-terraformers-ingestion"
-
     tags = {
         name = "ingestion_bucket"
         environment = "dev"
         description = "S3 bucket to store extracted raw tables from ToteSys."
+    }
+}
+
+## Processing S3 bucket
+resource "aws_s3_bucket" "processing_bucket" {
+    bucket = "nc-terraformers-processing"
+    tags = {
+        name = "processing_bucket"
+        environment = "dev"
+        description = "S3 bucket to store processed tables from Lambda2."
     }
 }
