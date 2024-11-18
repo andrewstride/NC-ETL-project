e2f7d785d4e2bd1a020bd16d4390c5d58ec0ab58
Daniel Solomon <danielsolomon@mailfence.com>
2024-11-18 12:29:19 +0000

    Refactoring/changing the lambda_handler, utils, and tests.

--- python/lambda1/test/test_week1_lambda.py
+++ python/lambda1/test/test_week1_lambda.py
@@ -4,9 +4,7 @@ from src.utils import (
     get_columns,
     write_to_s3,
     get_tables,
-    fetch_last_timestamps_from_db,
     read_timestamp_from_s3,
-    tables_and_timestamps_to_query,
     get_new_rows,
     write_df_to_csv,
     table_to_dataframe,
@@ -38,6 +36,31 @@ class TestGetDBCreds:
         for cred in creds:
             assert isinstance(creds[cred], str)
 
+
+class TestGetTables:
+    def test_get_tables_returns_list(self):
+        conn = db_connection()
+        tables = get_tables(conn)
+        assert isinstance(tables, list)
+
+    def test_get_tables_returns_tables(self):
+        conn = db_connection()
+        tables = get_tables(conn)
+        assert tables == [
+            "sales_order",
+            "transaction",
+            "department",
+            "staff",
+            "purchase_order",
+            "counterparty",
+            "payment",
+            "currency",
+            "payment_type",
+            "address",
+            "design",
+        ]
+
+
 @pytest.mark.skip
 class TestGetRows:
     def test_returns_list(self):
@@ -67,14 +90,15 @@ class TestGetColumns:
         result = get_columns(conn, "staff")
         assert len(result) == 7
 
-@pytest.mark.skip
+@pytest.mark.skip("Test needs to be refactored.")
 class TestLogger:
+    @mock_aws
     def test_token_logger(self):
         with LogCapture() as l:
             lambda_handler([], {})
             l.check_present(("root", "ERROR", "Houston, we have a major problem"))
 
-@pytest.mark.skip
+
 class TestWriteToS3:
     @mock_aws
     def test_returns_dict(self):
@@ -134,170 +158,73 @@ Invalid type for parameter Body, value: True, type: <class 'bool'>, valid types:
                 l
             )
 
-@pytest.mark.skip
-class TestGetTables:
-    def test_get_tables_returns_list(self):
-        conn = db_connection()
-        tables = get_tables(conn)
-        assert isinstance(tables, list)
 
-    def test_get_tables_returns_tables(self):
-        conn = db_connection()
-        tables = get_tables(conn)
-        assert tables == [
-            "sales_order",
-            "transaction",
-            "department",
-            "staff",
-            "purchase_order",
-            "counterparty",
-            "payment",
-            "currency",
-            "payment_type",
-            "address",
-            "design",
-        ]
-
-
-class TestFetchLastTimestamp:
+class TestReadTimestampFromS3:
+    @mock_aws
     def test_returns_dict(self):
-        conn = db_connection()
-        output = fetch_last_timestamps_from_db(conn)
+        s3 = boto3.client("s3")
+        table = "staff"
+        output = read_timestamp_from_s3(s3, table)
         assert isinstance(output, dict)
 
-    def test_all_tables_in_output(self):
-        conn = db_connection()
-        output = fetch_last_timestamps_from_db(conn)
-        tables = get_tables(conn)
-        for table in tables:
-            assert table in list(output.keys())
-
-
-class TestWritingTimestampTableToCSV:
-    def test_file_created_and_readable_to_dict(self):
-        conn = db_connection()
-        timestamp_dict = fetch_last_timestamps_from_db(conn)
-        with open("python/lambda1/test/test_timestamp_table.json", "w") as f:
-            f.write(json.dumps(timestamp_dict))
-        with open("python/lambda1/test/test_timestamp_table.json", "r") as f:
-            contents = json.load(f)
-        assert type(contents) == type(timestamp_dict)
-
-
-class TestReadTimestampsFromS3:
     @mock_aws
-    def test_returns_inputted_data(self):
-        data = json.dumps(
-            {
-                "sales_order": "2024-11-14 10:19:09.990000",
-                "transaction": "2024-11-14 10:19:09.990000",
-                "department": "2022-11-03 14:20:49.962000",
-                "staff": "2022-11-03 14:20:51.563000",
-                "purchase_order": "2024-11-14 09:11:09.922000",
-                "counterparty": "2022-11-03 14:20:51.563000",
-                "payment": "2024-11-14 10:19:09.990000",
-                "currency": "2022-11-03 14:20:49.962000",
-                "payment_type": "2022-11-03 14:20:49.962000",
-                "address": "2022-11-03 14:20:49.962000",
-                "design": "2024-11-14 09:41:09.839000",
-            }
-        )
-        client = boto3.client("s3")
-        client.create_bucket(
-            Bucket="test-bucket",
-            CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
-        )
-        write_to_s3(client, "test-bucket", "timestamp_table", "json", data)
-        output = read_timestamps_table_from_s3(
-            client, "test-bucket", "timestamp_table.json"
-        )
+    def test_returns_timestamp_dict(self):
+        s3 = boto3.client("s3")
+        s3.create_bucket(Bucket="nc-terraformers-ingestion",
+                         CreateBucketConfiguration={
+                             "LocationConstraint": "eu-west-2"})
+        data = json.dumps({"staff": "test_timestamp"})
+        s3.put_object(Bucket="nc-terraformers-ingestion",
+                      Body=data,
+                      Key="staff_timestamp.json")
+        table = "staff"
+        output = read_timestamp_from_s3(s3, table)
         assert output == json.loads(data)
-        assert isinstance(output, dict)
 
     @mock_aws
-    def test_handles_no_such_bucket_error(self):
-        client = boto3.client("s3")
-        with LogCapture() as l:
-            output = read_timestamps_table_from_s3(
-                client, "test-bucket", "timestamp_table.json"
-            )
-            assert output["result"] == "Failure"
-            assert """root ERROR
-  An error occurred (NoSuchBucket) when calling the GetObject operation: The specified bucket does not exist""" in (
-                str(l)
-            )
+    def test_handles_no_timestamp(self):
+        s3 = boto3.client("s3")
+        s3.create_bucket(Bucket="nc-terraformers-ingestion",
+                         CreateBucketConfiguration={
+                             "LocationConstraint": "eu-west-2"})
+        table = "staff"
+        output = read_timestamp_from_s3(s3, table)
+        assert output == {"detail" : "No timestamp exists"}
 
     @mock_aws
-    def test_handles_key_not_found_error(self):
-        client = boto3.client("s3")
-        client.create_bucket(
-            Bucket="test-bucket",
-            CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
-        )
+    def test_handles_error(self):
+        s3 = boto3.client("s3")
+        table = "staff"
         with LogCapture() as l:
-            output = read_timestamps_table_from_s3(
-                client, "test-bucket", "timestamp_table.json"
-            )
+            output = read_timestamp_from_s3(s3, table)
             assert output["result"] == "Failure"
             assert """root ERROR
-  An error occurred (NoSuchKey) when calling the GetObject operation: The specified key does not exist.""" in (
-                str(l)
+  An error occurred (NoSuchBucket) when calling the GetObject operation: The specified bucket does not exist""" in str(
+                l
             )
 
 
-class TestTablesAndTimestampsToQuery:
-    def test_returns_dict(self):
-        assert isinstance(tables_and_timestamps_to_query({}, {}), dict)
-
-    def test_finds_tables_to_update(self):
-        s3_table = {
-            "sales_order": "2023-11-14 10:19:09.990000",
-            "transaction": "2024-11-14 10:19:09.990000",
-            "department": "2022-11-03 14:20:49.962000",
-            "staff": "2022-11-03 14:20:51.563000",
-            "purchase_order": "2024-11-14 09:11:09.922000",
-            "counterparty": "2022-11-03 14:20:51.563000",
-            "payment": "2024-11-14 10:19:09.990000",
-            "currency": "2022-11-03 14:20:49.962000",
-            "payment_type": "2022-11-03 14:20:49.962000",
-            "address": "2022-11-03 14:20:49.962000",
-            "design": "2024-11-14 09:41:09.839000",
-        }
-        db_table = {
-            "sales_order": "2024-11-14 10:19:09.990000",
-            "transaction": "2024-11-14 10:19:09.990000",
-            "department": "2022-11-03 14:20:49.962000",
-            "staff": "2022-11-03 14:20:51.563000",
-            "purchase_order": "2024-11-14 09:11:09.922000",
-            "counterparty": "2022-11-03 14:20:51.563000",
-            "payment": "2024-11-14 10:19:09.990000",
-            "currency": "2022-11-03 14:20:49.962000",
-            "payment_type": "2022-11-03 14:20:49.962000",
-            "address": "2022-11-03 14:20:49.962000",
-            "design": "2024-11-14 09:41:09.839000",
-        }
-        output = tables_and_timestamps_to_query(db_table, s3_table)
-        assert output == {"sales_order": "2023-11-14 10:19:09.990000"}
-
-    def test_handles_key_error(self):
-        s3_table = {
-            "staff": "2023-11-14 10:19:09.990000",
-            "sales_order": "2023-11-14 10:19:09.990000",
-        }
-        db_table = {"staff": "2024-11-14 10:19:09.990000", "test": "test"}
-        with LogCapture() as l:
-            output = tables_and_timestamps_to_query(db_table, s3_table)
-            assert str({"KeyError": "'test'"}) in str(l)
-        assert output == {"staff": "2023-11-14 10:19:09.990000"}
-
-@pytest.mark.skip
 class TestGetNewRows:
-    def test_csv_reformat_and_upload_returns_list_of_lists(self):
+    def test_returns_list_of_lists(self):
         conn = db_connection()
         output = get_new_rows(conn, "staff", "2013-11-14 10:19:09.990000")
         assert isinstance(output, list)
         for item in output:
             assert isinstance(item, list)
+
+    def test_handles_incorrect_timestamp(self):
+        conn = db_connection()
+        with LogCapture() as l:
+            output = get_new_rows(conn, "staff", "incorrect timestamp")
+            assert '''{'S': 'ERROR', 'V': 'ERROR', 'C': '22007', 'M': 'invalid value "inco" for "YYYY"', 'D': 'Value must be an integer.', 'F': 'formatting.c', 'L': '2416', 'R': 'from_char_parse_int_len'}''' in str(l)
+        assert output == []
+
+    # test returns data after timestamp
+    # timestamp = 
+    # output = get_new_rows(conn, sales_orders, timestamp)
+    # columns = get_columns(conn, sales_orders)
+    # df = pd.DataFrame(output, columns=columns)
+    # assert df["last_updated"].min() >= timestamp
 
 @pytest.mark.skip
 class TestCsvReformatAndUpload:
@@ -313,7 +240,7 @@ class TestCsvReformatAndUpload:
                 Bucket=test_bucket,
                 CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
             )
-            output = csv_reformat_and_upload(client, test_rows, test_columns, test_name)
+            output = write_df_to_csv(client, test_rows, test_columns, test_name)
             assert isinstance(output, dict)
             assert isinstance(output["result"], str)
 
@@ -329,7 +256,7 @@ class TestCsvReformatAndUpload:
                 Bucket=test_bucket,
                 CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
             )
-            csv_reformat_and_upload(client, test_rows, test_columns, test_name)
+            write_df_to_csv(client, test_rows, test_columns, test_name)
             response = client.list_objects_v2(Bucket=test_bucket).get("Contents")
             bucket_files = [file["Key"] for file in response]
             if len(bucket_files) > 1:
@@ -348,7 +275,7 @@ class TestCsvReformatAndUpload:
                 Bucket=test_bucket,
                 CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
             )
-            output = csv_reformat_and_upload(client, test_rows, test_columns, test_name)
+            output = write_df_to_csv(client, test_rows, test_columns, test_name)
             assert output == {
                 "result": "Success",
                 "detail": "Converted to csv, uploaded to ingestion bucket",
@@ -370,10 +297,10 @@ class TestCsvReformatAndUpload:
                 Bucket=test_bucket,
                 CreateBucketConfiguration={"LocationConstraint": "eu-west-2"},
             )
-            output = csv_reformat_and_upload(client, test_rows, test_columns, test_name)
+            output = write_df_to_csv(client, test_rows, test_columns, test_name)
             assert output == {"result": "Failure"}
 
-
+@pytest.mark.skip
 class TestLambdaHandler:
     # @mock_aws
     def test_returns_200_response(self):
@@ -387,6 +314,7 @@ class TestLambdaHandler:
             print(item["Key"])
         assert response == 1
 
+
 class TestGetTimestampFromDataFrame:
     def test_returns_timestamp(self):
         conn = db_connection()
@@ -394,4 +322,18 @@ class TestGetTimestampFromDataFrame:
         columns = get_columns(conn, "sales_order")
         test_df = pd.DataFrame(rows, columns=columns)
         print(test_df['last_updated'].max())
-        test_df.head().to_json('test_table.json', orient='records')
+        test_df.head().to_json('test_table.json')
+
+# @pytest.mark.skip
+# class TestReadTimestampFromS3:
+    
+#     @mock_aws
+#     def test_(self):
+#         table = "staff"
+#         s3 = boto3.client("s3")
+#         s3.create_bucket(Bucket="nc-terraformers-ingestion",
+#                          CreateBucketConfiguration={
+#                              "LocationConstraint": "eu-west-2"})
+#         output = read_timestamp_from_s3(s3, table)
+#         print(output)
+#         assert output == 1
