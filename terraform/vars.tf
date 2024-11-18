303bd64377d9b8737fc89a87e82a0fd12bf4750d
studiobigli <acuddlytoy@googlemail.com>
2024-11-18 14:41:39 +0000

    Add Lambda2 variables

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,3 +1,9 @@
+## Exclude following directories from Lambda compilation
+variable "lambda_comp_exclude_list" {
+    type = list(string)
+    default = ["test", "src/__pycache__", ".pytest_cache"]
+}
+
 ## Name for lambda1
 variable "lambda1_name" {
   type = string
@@ -22,8 +28,27 @@ variable "lambda1_source_dir" {
     default = "../python/lambda1"
 }
 
-## Exclude following directories from Lambda compilation
-variable "lambda_comp_exclude_list" {
-    type = list(string)
-    default = ["test", "src/__pycache__", ".pytest_cache"]
+
+## Name for lambda2
+variable "lambda2_name" {
+  type = string
+  default = "lambda2"
+}
+
+## Deployment directory for lambda2
+variable "lambda2_deployment_dir" {
+    type = string
+    default = "../terraform-remote-deployment/lambda2"
+}
+
+## Deployment directory for lambda2 layer
+variable "lambda2_layer_deployment_dir" {
+    type = string
+    default = "../terraform-remote-deployment/lambda2-layer"
+}
+
+## Source directory for lambda2
+variable "lambda2_source_dir" {
+    type = string
+    default = "../python/lambda2"
 }
