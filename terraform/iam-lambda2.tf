955b591668293fe55ad43d9d6ce53581ccc03e83
studiobigli <acuddlytoy@googlemail.com>
2024-11-18 16:19:32 +0000

    add GET permission from ingestion for lambda2

--- terraform/iam-lambda2.tf
+++ terraform/iam-lambda2.tf
@@ -13,16 +13,37 @@ data "aws_iam_policy_document" "s3_put_object_document_lambda2" {
     }
 }
 
-## Policy for lambda2, for lambda2 to have the S3 PUT object permission
+## Policy document to GET an object from the INGESTION S3 bucket
+data "aws_iam_policy_document" "s3_get_object_document_lambda2" {
+    statement {
+        effect = "Allow"
+        actions = ["S3:GetObject"]
+        resources = ["${aws_s3_bucket.ingestion_bucket.arn}/*"]
+    }
+}
+
+## Policy for lambda2 to have the S3 PUT object permission to PROCESSING
 resource "aws_iam_policy" "s3_put_policy_for_lambda2" {
     name = "s3_put_policy_for_${var.lambda2_name}"
     policy = data.aws_iam_policy_document.s3_put_object_document_lambda2.json
+}
+
+## Policy for lambda2 to have the S3 GET objection permission from INGESTION
+resource "aws_iam_policy" "s3_get_policy_for_lambda2" {
+    name = "s3_get_policy_for_${var.lambda2_name}"
+    policy = data.aws_iam_policy_document.s3_get_object_document_lambda2.json
 }
 
 ## Attach the S3 PUT object policy to the lambda2 iam role
 resource "aws_iam_role_policy_attachment" "s3_put_object_attachment_for_lambda2" {
     role = aws_iam_role.role_for_lambda2.name
     policy_arn = aws_iam_policy.s3_put_policy_for_lambda2.arn
+}
+
+## Attach the S3 GET object from INGESTION policy to the lambda2 iam role
+resource "aws_iam_role_policy_attachment" "s3_get_object_attachment_for_lambda2" {
+    role = aws_iam_role.role_for_lambda2.name
+    policy_arn = aws_iam_policy.s3_get_policy_for_lambda2.arn
 }
 
 ## Policy for lambda2, for lambda2 to log to Cloudwatch
