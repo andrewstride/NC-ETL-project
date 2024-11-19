23c094947d88d8edd26fb4eed446892f1c8d1d52
studiobigli <acuddlytoy@googlemail.com>
2024-11-19 10:25:58 +0000

    Lambda3 IAM permissions set

--- terraform/iam-lambda3.tf
+++ terraform/iam-lambda3.tf
@@ -1,11 +1,11 @@
-## iam role for lambda2, for lambda2 to have the lambda assume role permission
-resource "aws_iam_role" "role_for_lambda2" {
-    name = "role_for_${var.lambda2_name}"
+## iam role for lambda3, for lambda3 to have the lambda assume role permission
+resource "aws_iam_role" "role_for_lambda3" {
+    name = "role_for_${var.lambda3_name}"
     assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
 }
 
-## Policy document to PUT an object in the PROCESSING S3 bucket (a way of portraying the JSON)
-data "aws_iam_policy_document" "s3_put_object_document_lambda2" {
+## Policy document to GET an object from the PROCESSING S3 bucket
+data "aws_iam_policy_document" "s3_get_object_document_lambda3" {
     statement {
         effect = "Allow"
         actions = ["S3:PutObject"]
@@ -13,56 +13,34 @@ data "aws_iam_policy_document" "s3_put_object_document_lambda2" {
     }
 }
 
-## Policy document to GET an object from the INGESTION S3 bucket
-data "aws_iam_policy_document" "s3_get_object_document_lambda2" {
-    statement {
-        effect = "Allow"
-        actions = ["S3:GetObject"]
-        resources = ["${aws_s3_bucket.ingestion_bucket.arn}/*"]
-    }
+## Policy for lambda3 to have the S3 GET object permission to PROCESSING
+resource "aws_iam_policy" "s3_get_policy_for_lambda3" {
+    name = "s3_get_policy_for_${var.lambda3_name}"
+    policy = data.aws_iam_policy_document.s3_get_object_document_lambda3.json
 }
 
-## Policy for lambda2 to have the S3 PUT object permission to PROCESSING
-resource "aws_iam_policy" "s3_put_policy_for_lambda2" {
-    name = "s3_put_policy_for_${var.lambda2_name}"
-    policy = data.aws_iam_policy_document.s3_put_object_document_lambda2.json
+## Attach the S3 GET object policy to the lambda3 iam role
+resource "aws_iam_role_policy_attachment" "s3_get_object_attachment_for_lambda3" {
+    role = aws_iam_role.role_for_lambda3.name
+    policy_arn = aws_iam_policy.s3_get_policy_for_lambda3.arn
 }
 
-## Policy for lambda2 to have the S3 GET objection permission from INGESTION
-resource "aws_iam_policy" "s3_get_policy_for_lambda2" {
-    name = "s3_get_policy_for_${var.lambda2_name}"
-    policy = data.aws_iam_policy_document.s3_get_object_document_lambda2.json
-}
-
-## Attach the S3 PUT object policy to the lambda2 iam role
-resource "aws_iam_role_policy_attachment" "s3_put_object_attachment_for_lambda2" {
-    role = aws_iam_role.role_for_lambda2.name
-    policy_arn = aws_iam_policy.s3_put_policy_for_lambda2.arn
-}
-
-## Attach the S3 GET object from INGESTION policy to the lambda2 iam role
-resource "aws_iam_role_policy_attachment" "s3_get_object_attachment_for_lambda2" {
-    role = aws_iam_role.role_for_lambda2.name
-    policy_arn = aws_iam_policy.s3_get_policy_for_lambda2.arn
-}
-
-## Policy for lambda2, for lambda2 to log to Cloudwatch
-resource "aws_iam_policy" "Cloudwatch_log_policy_for_lambda2" {
-    name = "Cloudwatch_log_policy_for_lambda2"
+## Policy for lambda3, for lambda3 to log to Cloudwatch
+resource "aws_iam_policy" "Cloudwatch_log_policy_for_lambda3" {
+    name = "Cloudwatch_log_policy_for_lambda3"
     policy = data.aws_iam_policy_document.Cloudwatch_log_document.json
 }
 
-## Attach the Cloudwatch log policy to the lambda2 iam role
-resource "aws_iam_role_policy_attachment" "Cloudwatch_log_attachment_for_lambda2" {
-    role = aws_iam_role.role_for_lambda2.name
-    policy_arn = aws_iam_policy.Cloudwatch_log_policy_for_lambda2.arn
+## Attach the Cloudwatch log policy to the lambda3 iam role
+resource "aws_iam_role_policy_attachment" "Cloudwatch_log_attachment_for_lambda3" {
+    role = aws_iam_role.role_for_lambda3.name
+    policy_arn = aws_iam_policy.Cloudwatch_log_policy_for_lambda3.arn
 }
 
-
-## iam policy for sns and attach to lambda2 iam role
-resource "aws_iam_role_policy" "role_policy_for_lambda2_sns" {
-  name = "role_policy_for_lambda2_sns"
-  role = aws_iam_role.role_for_lambda2.name
+## iam policy for sns and attach to lambda3 iam role
+resource "aws_iam_role_policy" "role_policy_for_lambda3_sns" {
+  name = "role_policy_for_lambda3_sns"
+  role = aws_iam_role.role_for_lambda3.name
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -91,16 +69,4 @@ resource "aws_iam_role_policy" "role_policy_for_lambda2_sns" {
       },
     ]
   })
-}
-
-## Policy for lambda2, for lambda2 to get secret value
-resource "aws_iam_policy" "get_secret_value_policy_for_lambda2" {
-    name = "get_secret_value_policy_for_${var.lambda2_name}"
-    policy = data.aws_iam_policy_document.get_secret_value_policy.json
-}
-
-## Attach the get secret value policy to the lambda2 iam role
-resource "aws_iam_role_policy_attachment" "get_secret_value_attachment_for_lambda2" {
-    role = aws_iam_role.role_for_lambda2.name
-    policy_arn = aws_iam_policy.get_secret_value_policy_for_lambda2.arn
 }
