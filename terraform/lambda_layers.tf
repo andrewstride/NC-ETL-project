8717b3ccc9fe8ad197746984e4c619739482aaf6
studiobigli <acuddlytoy@googlemail.com>
2024-11-20 10:20:40 +0000

    Remove unused local-exec code, add lambda3 layer

--- terraform/lambda_layers.tf
+++ terraform/lambda_layers.tf
@@ -1,21 +1,9 @@
-## pip install requirements for lambda1 (from requirements-lambda1.txt file)
-# resource "null_resource" "create_dependencies_lambda1" {
-#     provisioner "local-exec" {
-#         command = "pip install -r ${path.module}/../requirements-${var.lambda1_name}.txt -t ${path.module}/${var.lambda1_layer_deployment_dir}/python"
-#     }
-#     triggers = {
-#         dependencies = filemd5("${path.module}/../requirements-${var.lambda1_name}.txt")
-#     }
-# }
-
 ## zip file for lambda1 layer - getting the dependencies file locally and zipping it
-
 data "archive_file" "layer_code_for_lambda1" {
     type = "zip"
-
     output_path = "${path.module}/../terraform-remote-deployment/lambda1_layer.zip"
     source_dir = "${path.module}/${var.lambda1_layer_deployment_dir}"
-    # depends_on = [null_resource.create_dependencies_lambda1]
+
 }
 
 ## Create lambda1 layer from the zip file
@@ -25,24 +13,11 @@ resource "aws_lambda_layer_version" "layer_for_lambda1" {
     s3_key = aws_s3_object.lambda1_layer.key
 }
 
-## pip install requirements for lambda2 (from requirements-lambda2.txt file)
-# resource "null_resource" "create_dependencies_lambda2" {
-#     provisioner "local-exec" {
-#         command = "pip install -r ${path.module}/../requirements-${var.lambda2_name}.txt -t ${path.module}/${var.lambda2_layer_deployment_dir}/python"
-#     }
-#     triggers = {
-#         dependencies = filemd5("${path.module}/../requirements-${var.lambda2_name}.txt")
-#     }
-# }
-
 ## zip file for lambda2 layer - getting the dependencies file locally and zipping it
-
 data "archive_file" "layer_code_for_lambda2" {
     type = "zip"
-
     output_path = "${path.module}/../terraform-remote-deployment/lambda2_layer.zip"
     source_dir = "${path.module}/${var.lambda2_layer_deployment_dir}"
-    # depends_on = [null_resource.create_dependencies_lambda2]
 }
 
 ## Create lambda2 layer from the zip file
@@ -50,4 +25,19 @@ resource "aws_lambda_layer_version" "layer_for_lambda2" {
     layer_name = "layer_for_${var.lambda2_name}"
     s3_bucket = aws_s3_object.lambda2_layer.bucket
     s3_key = aws_s3_object.lambda2_layer.key
+}
+
+## zip file for lambda3 layer - getting the dependencies file locally and zipping it
+data "archive_file" "layer_code_for_lambda3" {
+    type = "zip"
+    output_path = "${path.module}/../terraform-remote-deployment/lambda3_layer.zip"
+    source_dir = "${path.module}/${var.lambda3_layer_deployment_dir}"
+
+}
+
+## Create lambda3 layer from the zip file
+resource "aws_lambda_layer_version" "layer_for_lambda3" {
+    layer_name = "layer_for_${var.lambda3_name}"
+    s3_bucket = aws_s3_object.lambda3_layer.bucket
+    s3_key = aws_s3_object.lambda3_layer.key
 }
