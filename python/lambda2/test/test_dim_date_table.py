051080240aff4df80afab0651745cf3b46cae240
Shahanaz Shaik <s.shaik1826@gmail.com>
2024-11-20 12:21:37 +0000

    Did some test working

--- python/lambda2/test/test_dim_date_table.py
+++ python/lambda2/test/test_dim_date_table.py
@@ -1,13 +1,14 @@
 
 import pytest
-import pandas
+import pandas as pd
 from src.dim_date_table import dim_date
 
 class TestDateTable:
+    def test_dim_date_table_type(self):
+        test = dim_date()
+        print(dir(test))
+        assert isinstance(test, pd.DataFrame)
+
     def test_dim_date_table_length(self):
         test = dim_date()
         assert len(test) == 9497
-    
-    def test_dim_date_table_type(self):
-        test = dim_date()
-        pass
