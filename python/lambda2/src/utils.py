7efb2bb16a94ed6c0176473ce734b447c29111f1
Shahanaz Shaik <s.shaik1826@gmail.com>
2024-11-20 14:21:15 +0000

    just extracted bucket details

--- python/lambda2/src/utils.py
+++ python/lambda2/src/utils.py
@@ -4,11 +4,16 @@ import boto3
 def read_data_from_s3_bucket():
     s3 = boto3.client("s3")
     response = s3.list_buckets()
-    ingested_bucket = response['Buckets'][1]['Name']
+    ingested_bucket = 'nc-terraformers-ingestion'
     return ingested_bucket
-    
 
 
+    # pre_star_schema_df = pd.read_csv(StringIO(latest_data))
+    # star_schema_df = pre_star_schema_df[[
+    #         "design_id",
+    #         "design_name",
+    #         "file_location",
+    #         "file_name"]].copy()
     #input bucket to read data from the s3 bucket
     #perfom dimensional table process
     #convert to parquet
