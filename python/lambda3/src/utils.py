fbbcbab9eaca949d212165eabbe17f13e2221d11
Andrew Stride <andrewjonathanstride@gmail.com>
2024-11-21 10:37:35 +0000

    add fastparquet to reqs, conftest configured

---
+++ python/lambda3/src/utils.py
@@ -0,0 +1,24 @@
+import logging
+import pandas as pd
+import json
+from io import BytesIO
+
+def get_parquet(s3, filename):
+    '''
+    Gets Parquet file from s3 bucket and returns as Pandas Dataframe
+    
+    Parameters:
+    s3: Boto3.client's3') connection
+    Filename (str): filename to collect
+    
+    Returns:
+    Pandas DataFrame'''
+    try:
+        logging.info(f"Reading {filename} from nc-terraformers-processing bucket")
+        response = s3.get_object(Bucket="nc-terraformers-processing", Key=filename)
+        body = response["Body"]
+        df = pd.read_parquet(BytesIO(body))
+        return df
+
+    except Exception as e:
+        logging.error(e)
--- python/lambda3/test/test_week3_lambda.py
+++ python/lambda3/test/test_week3_lambda.py
@@ -1,6 +1,8 @@
 from src.week3_lambda import lambda_handler
+from src.utils import get_parquet
+import pandas as pd
 
-class TestDummy:
-    def test_dummy_test(self):
-        lambda_handler([],[])
-        assert 1 == 1
+class TestGetParquet:
+    def test_returns_dataframe(self, nc_terraformers_processing_s3):
+        output = get_parquet(nc_terraformers_processing_s3, "test_staff.parquet")
+        assert isinstance(output, pd.DataFrame)
