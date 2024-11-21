068fa2472236e915d6849b218983320b37b4500b
studiobigli <acuddlytoy@googlemail.com>
2024-11-21 12:19:23 +0000

    add df_to_sql test

--- python/lambda3/test/test_week3_lambda.py
+++ python/lambda3/test/test_week3_lambda.py
@@ -1,25 +1,31 @@
 from src.week3_lambda import lambda_handler
-from src.utils import get_parquet
+from src.utils import import_pq_to_df, df_to_sql
 from testfixtures import LogCapture
 import pandas as pd
 
 class TestGetParquet:
     def test_returns_dataframe(self, nc_terraformers_processing_s3):
-        output = get_parquet(nc_terraformers_processing_s3, "test_staff.parquet")
+        output = import_pq_to_df(nc_terraformers_processing_s3, "test_staff.parquet")
         assert isinstance(output, pd.DataFrame)
 
     def test_df_unchanged(self, nc_terraformers_processing_s3, test_df):
-        output = get_parquet(nc_terraformers_processing_s3, "test_staff.parquet")
+        output = import_pq_to_df(nc_terraformers_processing_s3, "test_staff.parquet")
         assert output.equals(test_df)
 
     def test_handles_no_such_key_error(self, nc_terraformers_processing_s3):
         with LogCapture() as l:
-            output = get_parquet(nc_terraformers_processing_s3, "invalid_file.parquet")
+            output = import_pq_to_df(nc_terraformers_processing_s3, "invalid_file.parquet")
             assert output == {"result": "failure"}
             assert "An error occurred (NoSuchKey) when calling the GetObject operation: The specified key does not exist." in str(l)
 
     def test_logs_progress(self, nc_terraformers_processing_s3):
         with LogCapture() as l:
-            get_parquet(nc_terraformers_processing_s3, "test_staff.parquet")
+            import_pq_to_df(nc_terraformers_processing_s3, "test_staff.parquet")
             assert "Reading test_staff.parquet from nc-terraformers-processing bucket" in str(l)
             assert "test_staff.parquet file successfully imported into DataFrame" in str(l)
+
+class TestDataFrameToSQL:
+    def test_df_to_sql_returns_int(self, conn_fixture):
+        assert df_to_sql() == 1
+
+    
