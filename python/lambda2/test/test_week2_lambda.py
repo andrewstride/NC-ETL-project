c2d8952dd927fadfeec6d8d2e0da6529bd09dfca
Shahanaz Shaik <s.shaik1826@gmail.com>
2024-11-22 11:42:29 +0000

    Done staff and date table.

--- python/lambda2/test/test_week2_lambda.py
+++ python/lambda2/test/test_week2_lambda.py
@@ -1,16 +1,3 @@
-import pytest
-from unittest.mock import Mock, patch
-from datetime import datetime
 
-from src.utils import dim_staff
-
-class TestBucket:
-    @patch("src.utils.dim_staff")
-    def test_dim_staff_function_mock(self, mock_dim_staff):
-        mock_dim_staff.return_value = "called the mock"
-        mock_s3 = Mock()
-        result = mock_dim_staff(mock_s3)
-        mock_dim_staff.assert_called_once()
-        assert result == "called the mock"
     
 
